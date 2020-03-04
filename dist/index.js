435bee947264482fc4055611627f0ffce90db9c6
Penguin <say8425@gmail.com>
2020-03-05 02:15:45 +0900

    test

--- dist/index.js
+++ dist/index.js
@@ -50,15 +50,20 @@ const core = __webpack_require__(258)
 const axios = __webpack_require__(954)
 const telegramUrl = `https://api.telegram.org/bot635365494:${core.getInput('telegram_key')}/sendMessage`
 
-axios.post(telegramUrl, {
-  chat_id: core.getInput('chat_id'),
-  text: core.getInput('text')
-}).then(response => {
-  // core.setOutput('status', response.data.ok)
-  // core.setOutput('sent_at', new Date(response.data.result.date.toTimeString()))
-}).catch(error => {
-  // core.setFailed(error)
-})
+async function postTelegram () {
+  try {
+    const response = await axios.post(telegramUrl, {
+      chat_id: core.getInput('chat_id'),
+      text: core.getInput('text')
+    })
+    // core.setOutput('status', response.data.ok)
+    // core.setOutput('sent_at', new Date(response.data.result.date).toTimeString())
+  } catch (error) {
+    // core.setFailed(error)
+  }
+}
+
+postTelegram()
 
 
 /***/ }),
