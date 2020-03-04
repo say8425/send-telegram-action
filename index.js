dbc434e107e7e1658a857911ba37e1015b474080
Penguin <say8425@gmail.com>
2020-03-05 02:05:53 +0900

    test

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core')
 const axios = require('axios')
 const telegramUrl = `https://api.telegram.org/bot635365494:${core.getInput('telegram_key')}/sendMessage`
 
-axios.post(telegramUrl, {
-  chat_id: core.getInput('chat_id'),
-  text: core.getInput('text')
-}).then(response => {
-  core.setOutput('status', response.data.ok)
-  core.setOutput('sent_at', new Date(response.data.result.date.toTimeString()))
-}).catch(error => {
-  core.setFailed(error)
-})
+// axios.post(telegramUrl, {
+//   chat_id: core.getInput('chat_id'),
+//   text: core.getInput('text')
+// }).then(response => {
+//   core.setOutput('status', response.data.ok)
+//   core.setOutput('sent_at', new Date(response.data.result.date.toTimeString()))
+// }).catch(error => {
+//   core.setFailed(error)
+// })
