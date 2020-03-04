334047603dc7a9277c407462fd96747ed5140f53
Penguin <say8425@gmail.com>
2020-03-04 23:27:39 +0900

    use async

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const core = require('@actions/core')
 const axios = require('axios')
 const telegramUrl = `https://api.telegram.org/bot635365494:${core.getInput('telegram_key')}/sendMessage`
 
-axios.get('telegramUrl', {
-  chat_id: core.getInput('chat_id'),
-  text: core.getInput('text')
-}).then(response => {
-  core.setOutput('status', response.ok)
-  core.setOutput('sent_at', new Date(response.result.date))
-}).catch(error => {
-  core.setFailed(error)
-})
+async function postTelegram() {
+  try {
+    const response = axios.post(telegramUrl, {
+      chat_id: core.getInput('chat_id'),
+      text: core.getInput('text')
+    })
+    core.setOutput('status', response.ok)
+    core.setOutput('sent_at', new Date(response.result.date))
+  } catch (error) {
+    core.setFailed(error)
+  }
+}
+
+postTelegram()
