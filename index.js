c820bce4dbda5b7b0152db45c8027fbe674b3e75
Penguin <say8425@gmail.com>
2020-03-05 01:59:44 +0900

    use again then catch

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const core = require('@actions/core')
 const axios = require('axios')
 const telegramUrl = `https://api.telegram.org/bot635365494:${core.getInput('telegram_key')}/sendMessage`
 
-async function postTelegram () {
-  try {
-    const response = await axios.post(telegramUrl, {
-      chat_id: core.getInput('chat_id'),
-      text: core.getInput('text')
-    })
-    core.setOutput('status', response.data.ok)
-    core.setOutput('sent_at', new Date(response.data.result.date).toTimeString())
-  } catch (error) {
-    core.setFailed(error)
-  }
-}
-
-postTelegram()
+axios.post(telegramUrl, {
+  chat_id: core.getInput('chat_id'),
+  text: core.getInput('text')
+}).then(response => {
+  core.setOutput('status', response.data.ok)
+  core.setOutput('sent_at', new Date(response.data.result.date.toTimeString()))
+}).catch(error => {
+  core.setFailed(error)
+})
